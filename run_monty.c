7d547d58c676585d996e8147a7d8f058c8641027
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-01-22 14:36:22 +0000

    Stacks and Queues

--- run_monty.c
+++ run_monty.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 #include <string.h>
+#include <stdio.h>
 
 void free_tokens(void);
 unsigned int token_arr_len(void);
@@ -119,7 +120,7 @@ int run_monty(FILE *script_fd)
 	if (init_stack(&stack) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 
-	while (fgets(&line, &len, script_fd) != -1)
+	while (getline(&line, &len, script_fd) != -1)
 	{
 		line_number++;
 		op_toks = strtow(line, DELIMS);
